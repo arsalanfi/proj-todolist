bb979ad2d867220300ef104a9ddf4a58c178e390
milad <milad.rh2802@gmail.com>
2025-05-02 16:39:48 +0330

    end of tags

--- main.js
+++ main.js
@@ -80,23 +80,82 @@ window.addEventListener("DOMContentLoaded", () => {
   document.getElementById("task-add").addEventListener("click", () => {
     let taskName = document.getElementById("task-name").value;
     console.log(taskName);
-    console.log(document.getElementById("task-description").value);
-    console.log(span.textContent);
+    let taskDescription = document.getElementById("task-description").value;
+    console.log(taskDescription);
+    let textTag = span.textContent;
     empty.classList.add("hidden");
     form.classList.add("hidden");
     addBtn.classList.remove("hidden");
-    let div = document.createElement("div");
-    let check = document.createElement("input");
-    check.setAttribute("type", "checkbox");
-    div.classList.add(
+    let div1 = document.createElement("div");
+    let div2 = document.createElement("div");
+    let divInput = document.createElement("div");
+    let checkBox = document.createElement("input");
+    checkBox.setAttribute("type", "checkbox");
+    div1.classList.add(
       "w-full",
-      "h-[100px]",
+      "h-fit",
       "border",
       "border-[#E9E9E9]",
       "rounded-xl",
-      "mt-4"
+      "mt-4",
+      "py-[12px]",
+      "px-[16px]"
     );
-    div.appendChild(check);
-    document.getElementById("container").appendChild(div);
+    div2.classList.add("flex", "gap-4");
+    checkBox.classList.add(
+      //   "appearance-none",
+      "w-5",
+      "h-5",
+      "accent-[#007BFF]",
+      "rounded-[5px]",
+      "border",
+      "border-[#CCCCCC]",
+      "cursor-pointer",
+      "checked:border-none"
+    );
+    divInput.classList.add("flex", "flex-col", "gap-3", "w-full");
+
+    document.getElementById("container").appendChild(div1);
+    div1.appendChild(div2);
+    div2.appendChild(checkBox);
+    div2.appendChild(divInput);
+    //task-name
+    let one = document.createElement("p");
+    one.append(taskName);
+    one.classList.add("font-[600]", "text-[14px]", "text-[#242424]");
+    divInput.appendChild(one);
+    //task-tag
+    let two = document.createElement("p");
+    two.append(textTag);
+    two.classList.add(
+      "font-[700]",
+      "text-[10px]",
+      "flex",
+      "justify-center",
+      "items-center",
+      "w-fit",
+      "py-[2px]",
+      "px-[8px]",
+      "rounded-[4px]"
+    );
+    if (two.textContent == "پایین") {
+      two.classList.add("bg-[#C3FFF1]", "text-[#11A483]");
+    } else if (two.textContent == "متوسط") {
+      two.classList.add("bg-[#FFEFD6]", "text-[#FFAF37]");
+    } else if (two.textContent == "بالا") {
+      two.classList.add("bg-[#FFE2DB]", "text-[#FF5F37]");
+    }
+
+    divInput.appendChild(two);
+    //task-description
+    let three = document.createElement("p");
+    three.append(taskDescription);
+    three.classList.add("font-[400]", "text-[12px]", "text-[#7D7D7F]");
+    divInput.appendChild(three);
+    //menu
+    let menu = document.createElement("img");
+    menu.setAttribute("src", "./Img/threedot.svg");
+    menu.classList.add("justify-self-end", "self-start", "w-[4px]");
+    div2.appendChild(menu);
   });
 });
